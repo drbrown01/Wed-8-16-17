02bb6a9b6557060530700781e24c471f20a45e0d
Dan <drbrown01@yahoo.com>
2017-08-22 11:43:03 -0500

    added algo.html and algo.js logic

---
+++ algo.js
@@ -0,0 +1,17 @@
+Function print 1-100(){
+  var num = [i]
+  For( var i=1; i<101; i++){
+
+  If ( var num % 3===0){
+  print('Fizz');
+}
+  If ( var num % 5 ===0 ){
+  print('Buzz');
+}
+  If (var num % 5 ===0 && %3 ===0){
+  print (`FizzBuzz`);
+}
+  else
+    Console.log (num);
+}
+}
